3e401a1f8306979777f909b1a1daab218c4301cd
minitick <seansregularemail@gmail.com>
2021-01-17 16:07:04 -0700

    Too many changes for one commit
    - Added Log file path.
    - Added GetTimestamp() for ISO-8601 compliant timestamps.
    - Implemented Reverse() method.
    - Added CreateOutputFilePath() method.  Creates filenames with GUIDs to be concated later.

--- YTP.cs
+++ YTP.cs
@@ -1,9 +1,13 @@
 using System;
-using System.Diagnostics;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.Resources;
+using Xabe.FFmpeg;
+using System.Collections;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace YTP_Vinesauce_Edition
@@ -24,7 +28,7 @@ namespace YTP_Vinesauce_Edition
         private static readonly int NumEffects;
         private static Random EffectPicker = new Random();
 
-        private static Xabe.FFmpeg.FFmpeg _ffmpeg;
+        //private static Xabe.FFmpeg.FFmpeg _ffmpeg;
 
         [STAThread]
         /// <summary>
@@ -32,6 +36,16 @@ namespace YTP_Vinesauce_Edition
         /// </summary>
         static void Main()
         {
+            string LogFilePath = ".\\debug.log";
+
+            if (!File.Exists(LogFilePath))
+            {
+
+                using (StreamWriter sw = File.CreateText(LogFilePath))
+                {
+                    sw.WriteLine(GetTimestamp() + "\n");
+                }
+            }
 
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
@@ -39,15 +53,45 @@ namespace YTP_Vinesauce_Edition
             Application.Run(new Form1());
         }
 
-        static void Reverse(String InputFilePath)
+        /// <summary>
+        /// Returns datetime in the universal ISO-8601 format.  Primarily used
+        /// for writing out to the debug log.
+        /// </summary>
+        /// <returns></returns>
+        static String GetTimestamp()
         {
-            Debug.WriteLine(DateTime.Today.ToString("yyyy") "Reverse called");
+            string Timestamp = DateTime.Today.ToString("u");
 
-            // 
-            
+            return Timestamp;
+        }
 
+        private static async Task Reverse(string inputFile)
+        {
+            IMediaInfo mediaInfo = await FFmpeg.GetMediaInfo(inputFile);
+            Debug.Write("MediaInfo set\n");
 
-            return;
+            IStream videoStream = mediaInfo.VideoStreams.FirstOrDefault()
+                ?.SetCodec(VideoCodec.h264)
+                ?.Reverse()
+                ?.SetSize(VideoSize.Hd480);
+
+            Debug.Write("Finished videoStream\n");
+
+            var reverse = FFmpeg.Conversions.New()
+                .AddStream(videoStream)
+                .SetOutput(CreateOutputFilePath())
+                .SetOverwriteOutput(true);
+
+            await reverse.Start();
+
+            Debug.Write("Exiting Reverse()\n");
+        }
+
+        private static string CreateOutputFilePath()
+        {
+            Guid guid = Guid.NewGuid();
+
+            return Path.Combine(outputDirectory, guid.ToString() + ".mp4");
         }
     }
 
